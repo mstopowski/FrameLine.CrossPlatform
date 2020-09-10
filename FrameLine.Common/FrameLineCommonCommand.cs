16430fb1253830ed62b5df455915c99f895cf78c
msto02 <msto02@PC31609>
2020-09-10 15:00:57 +0200

    UserInteract class added

--- FrameLine.Common/FrameLineCommonCommand.cs
+++ FrameLine.Common/FrameLineCommonCommand.cs
@@ -97,7 +97,7 @@ namespace FrameLine.Common
                 }
             }
 
-            // Drawing frameline & adding labels
+            // Drawing frameline lines
             for (int i = 0; i < frameLine.polyPoints.Count; i++)
             {
                 var line1ID = doc.Objects.AddLine(new Line(new Point3d(frameLine.polyPoints[i][0], frameLine.polyPoints[i][1] - frameHeight / 2, 0),
@@ -153,46 +153,6 @@ namespace FrameLine.Common
             doc.Layers.FindIndex(doc.Layers.FindName(flineLayer.Name).Index).IsLocked = true;
 
             return Result.Success;
-        }
-    }
-
-    public class UserInteract
-    {  
-        int startFrame = 0;
-        int endFrame = 0;
-        int spacing = 0;
-        bool modify = false;
-        public bool zmienna = false;
-
-        RhinoList<Spacing> userSpacings = new RhinoList<Spacing>();
-
-        public UserInteract(ref RhinoList<Spacing> spacingsList)
-        {
-            this.userSpacings = spacingsList;
-        }
-
-        public void AskUser()
-        {
-            RhinoGet.GetInteger("Enter starting frame number", false, ref startFrame);
-            RhinoGet.GetInteger("Enter end frame number", false, ref endFrame);
-            RhinoGet.GetInteger("Enter typical spacing", false, ref spacing);
-            RhinoGet.GetBool("Do you want to insert local modification of spacing?", true, "No", "Yes", ref modify);
-
-            Spacing spaceMain = new Spacing(startFrame, endFrame, spacing);
-            userSpacings.Add(spaceMain);
-
-            while (modify)
-            {
-                RhinoGet.GetInteger("Enter starting frame of modification", false, ref startFrame);
-                RhinoGet.GetInteger("Enter end frame of modification", false, ref endFrame);
-                RhinoGet.GetInteger("Enter spacing of modification", false, ref spacing);
-
-                Spacing spaceMod = new Spacing(startFrame, endFrame, spacing);
-                userSpacings.Add(spaceMod);
-
-                RhinoGet.GetBool("Do you want to add another local modification?", true, "No", "Yes", ref modify);
-            }
-            this.zmienna = true;
         }
     }
 }
---
+++ FrameLine.Common/UserInteract.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Rhino;
+using Rhino.Collections;
+using Rhino.Input;
+
+namespace FrameLine.Common
+{
+    public class UserInteract
+    {
+        int startFrame = 0;
+        int endFrame = 0;
+        int spacing = 0;
+        bool modify = false;
+        public bool zmienna = false;
+
+        RhinoList<Spacing> userSpacings = new RhinoList<Spacing>();
+
+        public UserInteract(ref RhinoList<Spacing> spacingsList)
+        {
+            this.userSpacings = spacingsList;
+        }
+
+        public void AskUser()
+        {
+            RhinoGet.GetInteger("Enter starting frame number", false, ref startFrame);
+            RhinoGet.GetInteger("Enter end frame number", false, ref endFrame);
+            RhinoGet.GetInteger("Enter typical spacing", false, ref spacing);
+            RhinoGet.GetBool("Do you want to insert local modification of spacing?", true, "No", "Yes", ref modify);
+
+            Spacing spaceMain = new Spacing(startFrame, endFrame, spacing);
+            userSpacings.Add(spaceMain);
+
+            while (modify)
+            {
+                RhinoGet.GetInteger("Enter starting frame of modification", false, ref startFrame);
+                RhinoGet.GetInteger("Enter end frame of modification", false, ref endFrame);
+                RhinoGet.GetInteger("Enter spacing of modification", false, ref spacing);
+
+                Spacing spaceMod = new Spacing(startFrame, endFrame, spacing);
+                userSpacings.Add(spaceMod);
+
+                RhinoGet.GetBool("Do you want to add another local modification?", true, "No", "Yes", ref modify);
+            }
+            this.zmienna = true;
+        }
+    }
+}
