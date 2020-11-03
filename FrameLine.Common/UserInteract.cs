039fb3e2225fec89230132b40994bafeb53efa77
mstopowski <49496093+mstopowski@users.noreply.github.com>
2020-11-03 09:02:24 +0100

    improved user prompt with option toggle

--- FrameLine.Common/UserInteract.cs
+++ FrameLine.Common/UserInteract.cs
@@ -1,6 +1,7 @@
 ﻿using Rhino.Collections;
 using Rhino.Input;
 using Rhino.Commands;
+using Rhino.Input.Custom;
 
 namespace FrameLine.Common
 {
@@ -11,8 +12,9 @@ namespace FrameLine.Common
         int Spacing = 0;
         int StretchStart = 0;
         int StretchInterval = 0;
-        bool Modify = false;
-        bool Stretch = false;
+        bool Modify = true;
+        bool Stretch = true;
+        readonly bool Start = true;
 
         readonly bool ValidStart = false;
         readonly bool ValidEnd = false;
@@ -36,6 +38,31 @@ namespace FrameLine.Common
 
         public bool AskUser()
         {
+            var prompt = "This command will add frame line to the document - press ENTER to continue";
+            var go = new GetOption();
+            go.SetCommandPrompt(prompt);
+            go.AcceptNothing(true);
+
+            var modOption = new OptionToggle(false, "Off", "On");
+            var extOption = new OptionToggle(false, "Off", "On");
+            go.AddOptionToggle("Modification", ref modOption);
+            go.AddOptionToggle("Extension", ref extOption);
+
+            while (Start)
+            {
+                var res = go.Get();
+
+                if (res == GetResult.Nothing) // user pressed enter
+                    break;
+
+                if (res == GetResult.Cancel) // user pressed ESC
+                    return false;
+
+                var option = go.Option();
+                if (null == option)
+                    return false;
+            }
+
             while (!ValidStart)
             {
                 if (RhinoGet.GetInteger("Enter starting frame", false, ref StartFrame) == Result.Cancel) return UserPressedESC;
@@ -58,7 +85,7 @@ namespace FrameLine.Common
 
             while (!ValidSpac)
             {
-                if (RhinoGet.GetInteger("Enter typical Spacing in milimeters [mm]", false, ref Spacing) == Result.Cancel) return UserPressedESC;
+                if (RhinoGet.GetInteger("Enter typical frame spacing in milimeters [mm]", false, ref Spacing) == Result.Cancel) return UserPressedESC;
                 if (Spacing < 0)
                 {
                     Rhino.UI.Dialogs.ShowMessage("Spacing cannot be less than zero.", "Warning");
@@ -69,9 +96,7 @@ namespace FrameLine.Common
             Spacing spaceMain = new Spacing(StartFrame, EndFrame, Spacing);
             UserSpacings.Add(spaceMain);
 
-            if (RhinoGet.GetBool("Do you want to insert local modification?", true, "No", "Yes", ref Modify) == Result.Cancel) return UserPressedESC;
-
-            while (Modify)
+            while (modOption.CurrentValue && Modify)
             {
                 Modify = false;
                 while (!ValidStartMod)
@@ -96,7 +121,7 @@ namespace FrameLine.Common
 
                 while (!ValidSpacMod)
                 {
-                    if (RhinoGet.GetInteger("Enter Spacing for modification", false, ref Spacing) == Result.Cancel) return UserPressedESC;
+                    if (RhinoGet.GetInteger("Enter spacing for modification in milimeters [mm]", false, ref Spacing) == Result.Cancel) return UserPressedESC;
                     if (Spacing < 0)
                     {
                         Rhino.UI.Dialogs.ShowMessage("Spacing cannot be less than zero.", "Warning");
@@ -107,19 +132,18 @@ namespace FrameLine.Common
                 Spacing spaceMod = new Spacing(StartFrame, EndFrame, Spacing);
                 UserSpacings.Add(spaceMod);
 
-                if (RhinoGet.GetBool("Do you want to insert another local modification?", true, "No", "Yes", ref Modify) == Result.Cancel) return UserPressedESC;
+                if (RhinoGet.GetBool("Do you want to add another local modification?", true, "No", "Yes", ref Modify) == Result.Cancel) return UserPressedESC;
             }
 
-            if (RhinoGet.GetBool("Do you want to locally stretch frame line?", true, "No", "Yes", ref Stretch) == Result.Cancel) return UserPressedESC;
-
-            while (Stretch)
+            while (extOption.CurrentValue && Stretch)
             {
                 Stretch = false;
-                if (RhinoGet.GetInteger("Enter frame to stretch", false, ref StretchStart) == Result.Cancel) return UserPressedESC;
-                if (RhinoGet.GetInteger("Amount of frames to stretch", false, ref StretchInterval) == Result.Cancel) return UserPressedESC;
+                if (RhinoGet.GetInteger("Enter frame after which you want to insert stretch", false, ref StretchStart) == Result.Cancel) return UserPressedESC;
+                if (RhinoGet.GetInteger("Amount of frames of stretch", false, ref StretchInterval) == Result.Cancel) return UserPressedESC;
                 Stretch stretch = new Stretch(StretchStart, StretchInterval);
                 UserStretch.Add(stretch);
             }
+
             return UserFieldForm;
         }
     }
